16c6d3e79b1282897d06ef7834983c8644e9c3a8
Andrey Nechesov <fxcom@yandex.ru>
2017-07-12 14:46:15 +0700

    centra token v2.0

---
+++ cby_token.sol
@@ -0,0 +1,248 @@
+pragma solidity ^0.4.8;
+    
+   // ----------------------------------------------------------------------------------------------
+   // Developer Nechesov Andrey
+   // Enjoy. (c) PRCR.org ICO Platform 2017. The PRCR Licence.
+   // ----------------------------------------------------------------------------------------------
+    
+   // ERC Token Standard #20 Interface
+   // https://github.com/ethereum/EIPs/issues/20
+  contract ERC20Interface {
+      // Get the total token supply
+      function totalSupply() constant returns (uint256 totalSupply);
+   
+      // Get the account balance of another account with address _owner
+      function balanceOf(address _owner) constant returns (uint256 balance);
+   
+      // Send _value amount of tokens to address _to
+      function transfer(address _to, uint256 _value) returns (bool success);
+   
+      // Send _value amount of tokens from address _from to address _to
+      function transferFrom(address _from, address _to, uint256 _value) returns (bool success);
+   
+      // Allow _spender to withdraw from your account, multiple times, up to the _value amount.
+      // If this function is called again it overwrites the current allowance with _value.
+      // this function is required for some DEX functionality
+      function approve(address _spender, uint256 _value) returns (bool success);
+   
+      // Returns the amount which _spender is still allowed to withdraw from _owner
+      function allowance(address _owner, address _spender) constant returns (uint256 remaining);
+   
+      // Triggered when tokens are transferred.
+      event Transfer(address indexed _from, address indexed _to, uint256 _value);
+   
+      // Triggered whenever approve(address _spender, uint256 _value) is called.
+      event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+  }
+
+  /**
+   * Math operations with safety checks
+   */
+  library SafeMath {
+    function mul(uint a, uint b) internal returns (uint) {
+      uint c = a * b;
+      assert(a == 0 || c / a == b);
+      return c;
+    }
+
+    function div(uint a, uint b) internal returns (uint) {
+      // assert(b > 0); // Solidity automatically throws when dividing by 0
+      uint c = a / b;
+      // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+      return c;
+    }
+
+    function sub(uint a, uint b) internal returns (uint) {
+      assert(b <= a);
+      return a - b;
+    }
+
+    function add(uint a, uint b) internal returns (uint) {
+      uint c = a + b;
+      assert(c >= a);
+      return c;
+    }
+
+    function max64(uint64 a, uint64 b) internal constant returns (uint64) {
+      return a >= b ? a : b;
+    }
+
+    function min64(uint64 a, uint64 b) internal constant returns (uint64) {
+      return a < b ? a : b;
+    }
+
+    function max256(uint256 a, uint256 b) internal constant returns (uint256) {
+      return a >= b ? a : b;
+    }
+
+    function min256(uint256 a, uint256 b) internal constant returns (uint256) {
+      return a < b ? a : b;
+    }
+
+    function assert(bool assertion) internal {
+      if (!assertion) {
+        throw;
+      }
+    }
+  }
+   
+  contract ERC20 is ERC20Interface {
+      string public constant symbol = "Centra";
+      string public constant name = "Centra token";
+      uint8 public constant decimals = 18;      
+      uint256 maxTokens = 20000000*10**18; 
+      uint256 ownerSupply = maxTokens*3/10;
+      uint256 _totalSupply = ownerSupply;  
+      uint256 token_price = 1/400*10**18; 
+      uint ico_start = 501545600;
+      uint ico_finish = 1504224000;               
+
+      using SafeMath for uint;
+
+      event Mint(address indexed to, uint value);
+      event MintFinished();
+      bool public mintingFinished = false;
+      
+      // Owner of this contract
+      address public owner;
+   
+      // Balances for each account
+      mapping(address => uint256) balances;
+   
+      // Owner of account approves the transfer of an amount to another account
+      mapping(address => mapping (address => uint256)) allowed;
+   
+      // Functions with this modifier can only be executed by the owner
+      modifier onlyOwner() {
+          if (msg.sender != owner) {
+              throw;
+          }
+          _;
+      }      
+   
+      // Constructor
+      function ERC20() {
+          owner = msg.sender;
+          balances[owner] = ownerSupply;
+      }
+   
+      function totalSupply() constant returns (uint256 totalSupply) {
+          totalSupply = _totalSupply;
+      }
+
+      //Withdraw money from contract balance to owner
+      function withdraw() onlyOwner returns (bool result) {
+          owner.send(this.balance);
+          return true;
+      }
+   
+      // What is the balance of a particular account?
+      function balanceOf(address _owner) constant returns (uint256 balance) {
+          return balances[_owner];
+      }
+   
+      // Transfer the balance from owner's account to another account
+      function transfer(address _to, uint256 _amount) returns (bool success) {
+
+          if(now < ico_start) throw;
+
+          if (balances[msg.sender] >= _amount 
+              && _amount > 0
+              && balances[_to] + _amount > balances[_to]) {
+              balances[msg.sender] -= _amount;
+              balances[_to] += _amount;
+              Transfer(msg.sender, _to, _amount);
+              return true;
+          } else {
+              return false;
+          }
+      }
+   
+      // Send _value amount of tokens from address _from to address _to
+      // The transferFrom method is used for a withdraw workflow, allowing contracts to send
+      // tokens on your behalf, for example to "deposit" to a contract address and/or to charge
+      // fees in sub-currencies; the command should fail unless the _from account has
+      // deliberately authorized the sender of the message via some mechanism; we propose
+      // these standardized APIs for approval:
+      function transferFrom(
+          address _from,
+          address _to,
+          uint256 _amount
+     ) returns (bool success) {
+
+         if(now < ico_start) throw;
+
+         if (balances[_from] >= _amount
+             && allowed[_from][msg.sender] >= _amount
+             && _amount > 0
+             && balances[_to] + _amount > balances[_to]) {
+             balances[_from] -= _amount;
+             allowed[_from][msg.sender] -= _amount;
+             balances[_to] += _amount;
+             Transfer(_from, _to, _amount);
+             return true;
+         } else {
+             return false;
+         }
+     }
+  
+     // Allow _spender to withdraw from your account, multiple times, up to the _value amount.
+     // If this function is called again it overwrites the current allowance with _value.
+     function approve(address _spender, uint256 _amount) returns (bool success) {
+         allowed[msg.sender][_spender] = _amount;
+         Approval(msg.sender, _spender, _amount);
+         return true;
+     }
+  
+     function allowance(address _owner, address _spender) constant returns (uint256 remaining) {
+         return allowed[_owner][_spender];
+     }
+
+    modifier canMint() {
+      if(mintingFinished) throw;
+      _;
+    }
+
+     /**
+     * @dev Function to mint tokens
+     * @param _to The address that will recieve the minted tokens.
+     * @param _amount The amount of tokens to mint.
+     * @return A boolean that indicates if the operation was successful.
+     */
+     /*
+     function mint(address _to, uint _amount) onlyOwner canMint returns (bool) {
+        totalSupply = totalSupply.add(_amount);
+        balances[_to] = balances[_to].add(_amount);
+        Mint(_to, _amount);
+        return true;
+      }
+      */
+      /**
+      * Buy tokens 
+      */
+      function tokens_buy() payable returns (bool) { 
+
+        if((now < ico_start)||(now >ico_finish)) throw;
+
+        if(_totalSupply >= maxTokens) throw;
+        if(!(msg.value >= token_price)) throw;
+        uint tokens_buy = msg.value/token_price*10**18;
+        if(!(tokens_buy > 0)) throw;
+        if(_totalSupply + tokens_buy > maxTokens) throw;
+        _totalSupply = _totalSupply.add(tokens_buy);
+        balances[msg.sender] = balances[msg.sender].add(tokens_buy);        
+        return true;
+      }
+
+      /**
+       * @dev Function to stop minting new tokens.
+       * @return True if the operation was successful.
+       */
+       /*
+      function finishMinting() onlyOwner returns (bool) {
+        mintingFinished = true;
+        MintFinished();
+        return true;
+      }
+      */
+ }
